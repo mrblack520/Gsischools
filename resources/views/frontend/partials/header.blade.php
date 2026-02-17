ae7b09566495a1fc6c77d4045e022b2e072e1c2b
muhammadibrahim-dorld <muhammadibrahim.dorld@gmail.com>
2026-02-17 14:08:48 -0800

    about page is done

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -2,63 +2,55 @@
     <div class="container" bis_skin_checked="1">
         <a class="Header__logo Header__logo--white-text" href="/"><img src="/assets/images/gsilogo.png"
                 alt="header-logo" class="img-fluid header-logo"></a>
-        <nav class="Header__menu">
-            <ul class="Header__menu-list">
-                <li class="Header__menu-list-item"><a href="{{ route('frontend.home') }}" rel="nofollow">Home</a> </li>
-                {{-- <li class="Header__menu-list-item"><a href="{{ route('frontend.next-gen') }}" rel="nofollow">Next --}}
-                        {{-- Gen</a></li> --}}
-                {{-- <li class="Header__menu-list-item"><a href="{{ route('frontend.aficionado') }}"
-                        rel="nofollow">Aficionado</a></li> --}}
-                {{-- <li class="Header__menu-list-item"><a href="{{ route('frontend.university') }}"
-                        rel="nofollow">University</a></li> --}}
-                <li class="Header__menu-list-item"><a href="{{ route('frontend.profession') }}"
-                        rel="nofollow">Events</a>
-                </li>
-                {{-- <li class="Header__menu-list-item"><a href="{{ route('frontend.profile') }}" rel="nofollow">Account</a> --}}
-                </li>
-                <li class="Header__menu-list-item"><a href="{{ route('frontend.about') }}" rel="nofollow">About</a></li>
-                <li class="Header__menu-list-item"><a href="{{ route('frontend.faqs') }}">FAQs</a></li>
-                <li class="Header__menu-list-item"><a
-                                        href="{{ route('frontend.privacy-policy') }}">Policies</a></li>
-                <!-- <li class="Header__menu-list-item"><a href="{{ route('frontend.example-question') }}" rel="nofollow">Explore<img
-                                class="arrow-down" src="./assets/images/arrow-down.svg" alt=""></a> </li> -->
-                {{-- <li>
-                    <div class="home-dropdown-menu">
-                        <div class="dropdown-btn">
-                            <span class="dropdown-btn-text">Explore</span>
-                            <svg role="img" viewBox="0 0 512 512">
-                                <path
-                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                            </svg>
-                        </div>
-                        <ul class="dropdown-options">
-                            <li class="option">
-                                <span class="option-text"><a href="{{ route('frontend.curiosity-hub') }}">Curiosity
-                                        hub</a></span>
-                            </li>
-                            <li class="option">
-                                <span class="option-text"><a href="{{ route('frontend.aficionado') }}">Available
-                                        Aficionados</a></span>
-                            </li>
-                            <li class="option">
-                                <span class="option-text"><a href="{{ route('frontend.example-question') }}">Example
-                                        Questions</a></span>
-                            </li>
-                            <li class="option">
-                                <span class="option-text"><a href="{{ route('frontend.faqs') }}">FAQs</a></span>
-                            </li>
-                            <li class="option">
-                                <span class="option-text"><a
-                                        href="{{ route('frontend.privacy-policy') }}">Policies</a></span>
-                            </li>
-                        </ul>
-                    </div>
-                </li> --}}
+        <nav class="Header__menu navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul class="Header__menu-list">
+                        <a><li class="nav-link active" aria-current="page" href="{{ route('frontend.home') }}">Home</li></a>
+                        <li class="Header__menu-list-item"><a href="{{ route('frontend.home') }}"
+                                rel="nofollow">Home</a>
+                        </li>
 
-            </ul>
+                        <li class="Header__menu-list-item"><a href="{{ route('frontend.profession') }}"
+                                rel="nofollow">Events</a>
+                        </li>
+
+                        </li>
+                        <li class="Header__menu-list-item"><a href="{{ route('frontend.about') }}"
+                                rel="nofollow">About</a>
+                        </li>
+                        <li class="Header__menu-list-item"><a href="{{ route('frontend.faqs') }}">FAQs</a></li>
+                        <li class="Header__menu-list-item"><a href="{{ route('frontend.privacy-policy') }}">Policies</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         </nav>
         <div class="Header__user-box" bis_skin_checked="1">
+
             <side-nav />
         </div>
     </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
 </header>
