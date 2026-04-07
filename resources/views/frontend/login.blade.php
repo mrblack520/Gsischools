15e60384b388379b457beb5e4ac0823050369b25
Raja-Huzaifa22 <huzaifa.dorld@gmail.com>
2026-04-08 03:38:58 +0500

    test

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (data.status || data.success) {
                 // 🔥 LOGIN SUCCESS
                 console.log(data)
-           window.location.href = '/token-login?token=' + data.token;
+        //    window.location.href = '/token-login?token=' + data.token;
             } else {
                 alert('Invalid login');
             }
