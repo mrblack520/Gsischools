baaf6419011fa58556c1589fd67426ce3b589092
mubashir <mubashir.dorld@gmail.com>
2026-04-15 02:57:01 +0500

    fix

--- portal/routes/web.php
+++ portal/routes/web.php
@@ -18,13 +18,16 @@ use Illuminate\Support\Facades\Redirect;
 
 Route::middleware(['web'])->get('/token-login', function(Request $request) {
     $token = $request->token; // API se mile token
-    dd($request);
     
     $response = Http::withHeaders([
         'Authorization' =>  $token
         ])->get(config('app.api_url').'/me'); // ya tumhara endpoint jahan user data milta
-        dd(config('app.api_url'));
-                
+        
+    dd('before api call');
+
+$response = Http::timeout(5)->get('https://google.com');
+
+dd('after api call'); 
 
     $user = User::where('email', $userData['email'])->first();
     if (!$userData) {
