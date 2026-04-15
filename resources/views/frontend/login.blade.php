65181c4a637e37cfc412bc803e4ae6e8a674ebdf
mirmohiudin <mohiudin.dorld@gmail.com>
2026-04-16 00:18:17 +0500

    test

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -13,20 +13,20 @@
                         <h3>Login to <strong>GSI</strong></h3>
                     </div>
                     <form id="loginForm">
-    <div class="form-group">
-        <label>Email</label>
-        <input type="email" id="email" class="form-control input-control-input" placeholder="Enter your email">
-    </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" id="email" class="form-control input-control-input" placeholder="Enter your email">
+                        </div>
 
-    <div class="form-group">
-        <label>Password</label>
-        <input type="password" id="password" class="form-control input-control-input" placeholder="Enter your password">
-    </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" id="password" class="form-control input-control-input" placeholder="Enter your password">
+                        </div>
 
-    <div class="input-control mt-3">
-        <button type="submit" class="btn input-control-input">Sign In</button>
-    </div>
-</form>
+                        <div class="input-control mt-3">
+                            <button type="submit" class="btn input-control-input">Sign In</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
 
-        fetch('/portal/api/login', {
+        fetch('/portal/api/login-gsi', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
