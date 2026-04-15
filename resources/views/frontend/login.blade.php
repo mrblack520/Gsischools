822018c4c669e6beb8bdf2f63485defe57e8b49c
mirmohiudin <mohiudin.dorld@gmail.com>
2026-04-16 02:31:28 +0500

    test

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
 
-        fetch('/portal/api/loginapi', {
+        fetch('/api/loginapi', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
