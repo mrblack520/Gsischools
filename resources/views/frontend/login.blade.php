ad3deac9f6b58d763537c832c7fbe125871f358f
mirmohiudin <mohiudin.dorld@gmail.com>
2026-04-16 03:44:01 +0500

    fix

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         console.log("Login Attempt:", email);
 
-     const response = await fetch('https://gsischools.com/portal/api/loginapi', {
+     const response = await fetch('https://gsischools.com/api/loginapi', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
