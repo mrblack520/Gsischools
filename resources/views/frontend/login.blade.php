3bbc9674cfb35f1e4b7d1331180e9482773c2cf9
mirmohiudin <mohiudin.dorld@gmail.com>
2026-04-16 02:41:50 +0500

    test

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
 
-        fetch('https://gsischools/portal/api/loginapi', {
+        fetch('https://gsischools.com/portal/api/loginapi', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
