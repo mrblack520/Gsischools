05fc0b00b6a005cd940c023953bc2553167b3507
mubashir <mubashir.dorld@gmail.com>
2026-04-29 22:54:08 +0500

    fix

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
             try {
                 data = JSON.parse(responseText);
             } catch(e) {
-                throw new Error('Server ne galat response diya. Please try again.');
+                throw new Error('Please try again.');
             }
 
             if (data.status && data.auto_login_url) {
